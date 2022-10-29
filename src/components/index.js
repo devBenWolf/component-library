463a8024bcae5c39e845df865b9b7c778ae44e4f
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-10-29 16:55:31 -0400

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Checkbox from "./Checkbox"
+import CustomCheckbox from "./CustomCheckbox"
+import SelectMenu from "./SelectMenu"
+import TextInput from "./TextInput"
+import TextArea from "./TextArea"
+import Radio from "./Radio"
+
+export {Checkbox, CustomCheckbox, SelectMenu, TextInput, TextArea, Radio}
