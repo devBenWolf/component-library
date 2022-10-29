da6363e8067e098c47bcea036f3cbb69e281b46f
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-10-29 16:57:12 -0400

    Update SelectMenu.js
    changed background-color,
    width,
    font-size,
    color,
    letter-spacing

--- src/components/SelectMenu.js
+++ src/components/SelectMenu.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const SelectMenu = () => {
     return ( 
         <Wrapper>
-        <label for="dogs">
-            <select name="dogs">
+        <label className="input-label">
+            <select name="selectMenu">
                 <option value="atka">Atka</option>
                 <option value="yakone">Yakone</option>
             </select>
@@ -18,23 +18,33 @@ export default SelectMenu;
 
 const Wrapper = styled.div`
     display: flex;
-    width: 20rem;
-    color: var(--clr-black);
+    width: 30rem;
     font-size: 1.2rem;
     font-weight: bold;
     line-height: 1.1;
     margin-top: 1rem;
     font-family: var(--font-normal-roboto);
 
+
     select {
         border: none;
-        background-color: var(--clr-medium-sapphire);
-        color: var(--clr-white);
-        width: 5rem;
-        font-size: 1.1rem;
-        box-shadow: 4px 3px 8px 1px;
+        background-color: var(--clr-light-blue);
+        width: fit-content;
+        font-size: 1.2rem;
         border-radius: 3px;
         margin-right: 0.5rem;
+        color: var(--clr-white);
+        letter-spacing: 1px;
     }
 
+    label {
+        font-size: 1.5rem;
+        font-weight: bold;
+        line-height: 1.1;
+        display: flex;        
+        gap: 3rem;
+        margin-top: 2rem;
+        font-family: var(--font-normal-roboto);
+        
+    }
 `
