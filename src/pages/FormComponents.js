8883dc535efe9e51345ced5cf4815f663b2e693a
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-10-29 16:56:05 -0400

    Create FormComponents.js

---
+++ src/pages/FormComponents.js
@@ -0,0 +1,16 @@
+import {Checkbox, CustomCheckbox, Radio, SelectMenu, TextArea, TextInput} from "../components/index"
+
+const FormComponents = () => {
+    return ( 
+        <div>
+            <Checkbox />
+            <CustomCheckbox />
+            <Radio />
+            <SelectMenu />
+            <TextInput />
+            <TextArea />
+        </div>
+     );
+}
+ 
+export default FormComponents;
